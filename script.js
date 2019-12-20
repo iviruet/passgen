07d5c7799edaac0df0bfed8d4f8244a4b309b808
IV <irisviruet@hotmail.com>
2019-12-19 23:10:27 -0500

    unfinished confirm prompts

---
+++ script.js
@@ -0,0 +1,43 @@
+//generates password 
+function generate() {
+
+    //password length/complexity
+    let complexity = document.getElementById("selector").value;
+    //possible password values    
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+    let password = "";
+
+    let result = confirm("Would you like uppercase characters?");
+    let result = confirm("Would you like to use lowercase characters?");
+    let result = confirm("Would you like to use special characters?");
+    let result = confirm("Would you like to use numbers?");
+
+    for (var i = 0; i <= complexity; i++) {
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+
+    document.getElementById("display").value = password;
+
+    document.getElementById("lastPasswords").innerHTML += password + "<br />";
+
+}
+// default length display at 25
+document.getElementById("length").innerHTML = "Length: 25";
+
+// length based on slider position
+document.getElementById("selector").oninput = function () {
+
+    if (document.getElementById("selector").value > 0) {
+        document.getElementById("length").innerHTML = "Length:" + document.getElementById("slider").value;
+    }
+    else {
+        document.getElementById("length").innerHTML = "Length: 1";
+    }
+}
+//copy password to clipboard
+function copyPassword() {
+    document.getElementById("display").select();
+    document.execCommand("Copy");
+
+    alert("Password successfully copied to clipboard!");
+}
