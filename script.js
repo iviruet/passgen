88ccb9b20f117ced332ff1376576e58ba77af43f
IV <irisviruet@hotmail.com>
2020-02-02 10:06:22 -0500

    confirms fixed, minor style change

--- script.js
+++ script.js
@@ -4,11 +4,31 @@ function generate() {
     //password length/complexity
     let complexity = document.getElementById("selector").value;
     //possible password values    
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+    let values = "";
+    let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lower = "abcdefghijklmnopqrstuvwxyz";
+    let nums = "1234567890";
+    let special = "!@#$%^&*()_+";
     let password = "";
 
-    let result = confirm("Would you like uppercase characters?");
-    
+    let upperBoolean = confirm("Would you like uppercase characters?");
+    let lowerBoolean = confirm("Would you like lowercase characters?");
+    let numsBoolean = confirm("Would you like numbers characters?");
+    let specialBoolean = confirm("Would you like special characters?");
+
+    if (upperBoolean) {
+        values += upper;
+    }
+    if (lowerBoolean) {
+        values += lower;
+    }
+    if (numsBoolean) {
+        values += nums;
+    }
+    if (specialBoolean) {
+        values += special;
+    }
+    ///////////create 
 
     for (var i = 0; i <= complexity; i++) {
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
